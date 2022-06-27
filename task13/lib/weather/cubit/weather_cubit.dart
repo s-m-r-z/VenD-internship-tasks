dae483eba041c031c9d60946bda804fbae00c171
s-m-r-z <59144787+s-m-r-z@users.noreply.github.com>
2022-06-27 18:15:01 +0500

    REMOVED BUILD FILES

--- task13/lib/weather/cubit/weather_cubit.dart
+++ task13/lib/weather/cubit/weather_cubit.dart
@@ -87,13 +87,6 @@ class WeatherCubit extends Cubit<WeatherState> {
       );
     }
   }
-
-  @override
-  WeatherState fromJson(Map<String, dynamic> json) =>
-      WeatherState.fromJson(json);
-
-  @override
-  Map<String, dynamic> toJson(WeatherState state) => state.toJson();
 }
 
 extension on double {
